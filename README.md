d9a0c6252e47157f48b5c2bde68c12d612ba10c8
Romario Lima <r_s.lima@live.it>
2013-08-04 05:58:25 -0300

    Added Fancy Header, README

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-indexer
-=======
+# CSS Indexer
+
+Automatically index the important sections of your CSS.
+
+## How to Use
+
+Just...
+
+    index.py $FILE_NAME
+
+`example.scss`
+
+    // == GENERAL
+    body { background:#FFF; }
+
+    // == TYPOGRAPHY
+    p { color:#000; }
+
+    // == AND SO ON...
+
+Will have this fancy header: 
+
+    /**
+      * ========= TABLE OF CONTENTS =========
+      * 
+      * 01. GENERAL
+      * 02. TYPOGRAPHY
+      * 03. AND SO ON...
+      *
+      */ 
+     
+
+
+Ideas? Fixes? Feel free to contribute!
--- index.py
+++ index.py
@@ -1,53 +1,73 @@
-
 import re
 import os
 import sys
-
+ 
 try:
     if not os.path.exists(sys.argv[1]):
 	    sys.stderr.write('ERROR: File %s was not found!' % sys.argv[1])
 	    sys.exit(1)
 except IndexError:
     print 'Type the filename after index.py'  
-
-
+ 
+ 
 def format_line(line, number):
 	return line.rstrip()
-
+ 
 def open_file():
 	return open( sys.argv[1], "r+" )
-
+ 
 def get_indexes():
 	comments = []
 	f = open_file()
 	regex = re.compile('\/\/\s\=\=\s([\s\S]+)')
 	count = 1
-
+ 
 	for line in f:	
 		res = regex.match(line)
 		if res:
-			comments.append({"text" : format_line(res.group(1), 
-				count),"line" : count})
+			comments.append({"text" : format_line(res.group(1), count)})
 		count += 1
 	f.close()
 	return comments
-
-def format_line_output(text,line):
-	return "// "+text+" at line "+str(line)+"\n"
-
+ 
+def format_line_output(text,index):
+	menuOpt = "0" + str(index) if index < 10 else str(index)
+	return "  * " + menuOpt + ". " + text + "\n"
+ 
 def markup(indexes):
-	original_file_content = ""
+	original_file_content = headerContent = headerTop = headerFooter = ""
+
 	with open_file() as f:
 	    original_file_content = f.read()
-	with open( sys.argv[1], "wb+" ) as f:
-		for i in indexes:
-			f.write(format_line_output(i['text'], i['line']))
-		f.write("\n\n")
-		f.write(original_file_content)
 
+
+	with open( sys.argv[1], "wb+" ) as f:
+
+
+		headerTop = "/**\n  * ========= TABLE OF CONTENTS =========\n  * \n"
+
+		for idx,i in enumerate(indexes):
+
+			headerContent += format_line_output(i['text'],idx+1)
+
+		headerFooter = "  *\n  */\n\n"
+
+		try:
+			f.write(headerTop)
+			f.write(headerContent)
+			f.write(headerFooter)
+			f.write(original_file_content)
+			print 'CSS Indexed! :)'
+		except :
+			sys.stderr.write('Something went Wrong :(')
+			# ? WILL THIS WORK ?
+			f.write(original_file_content)
+
+
+ 
 def clear():
 	#clear previous indexes
 	pass
-
+ 
 clear()
 markup(get_indexes())
